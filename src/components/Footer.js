1c840e24e594eddad359686514655bf0634ddecf
Luis Fernando Murara <murara@gmail.com>
2021-07-29 09:21:02 -0300

    fix: :bug: hide social links when blank (#14)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,24 +2,30 @@ import React from 'react'
 import * as styles from './Footer.module.scss'
 import config from '../../data/SiteConfig'
 
+const isPresent = string => string && string.trim() != '';
+
 const Footer = () => (
   <footer>
     <div className={styles.container}>
       <div>
-        <a
-          href={`https://twitter.com/${config.userTwitter}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Twitter
-        </a>
-        <a
-          href={`https://github.com/${config.userGitHub}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub
-        </a>
+        {isPresent(config.userTwitter) &&
+          <a
+            href={`https://twitter.com/${config.userTwitter}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Twitter
+          </a>
+        }
+        {isPresent(config.userGitHub) &&
+          <a
+            href={`https://github.com/${config.userGitHub}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        }
         <a
           href={config.siteUrl + config.siteRss}
           target="_blank"
